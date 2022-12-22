447cdbcb424ae5118ee506d89f101c4eb3371fd9
Theo Joignant <tjoignant@gmail.com>
2022-12-22 12:23:04 +0100

    Added normalize_data function

--- functions.py
+++ functions.py
@@ -79,6 +79,25 @@ def MC_Pricing(strike: float, barrier: float, S0: float, v0: float, risk_free_ra
 
 def LSM_dataset(strike: float, barrier: float, v0: float, risk_free_rate: float, maturity: float, rho: float,
                kappa: float, theta: float, sigma: float, nb_steps=252, nb_simuls=100000, seed=1):
+    """
+    Inputs:
+     - strike         : american D&O Call strike (float)
+     - barrier        : american D&O Call barrier (float)
+     - v0             : initial asset variance (float)
+     - risk_free_rate : yearly asset continuous drift (perc)
+     - maturity       : yearly duration of simulation (float)
+     - rho            : correlation between asset returns and variance (float)
+     - kappa          : rate of mean reversion in variance process (float)
+     - theta          : long-term mean of variance process (float)
+     - sigma          : vol of vol / volatility of variance process (float)
+     - nb_steps       : number of time steps (int)
+     - nb_simuls      : number of simulations (int)
+     - seed           : random seed (int)
+    Outputs:
+     - training samples (1D array)
+     - labels (1D array)
+     - pathwise differentials (1D array)
+    """
     seed_list = np.arange(seed, nb_simuls + seed)
     X_list = np.linspace(10, 200, nb_simuls)
     Y_list = []
@@ -90,3 +109,32 @@ def LSM_dataset(strike: float, barrier: float, v0: float, risk_free_rate: float,
                                                       nb_simuls=1, seed=seed)
         Y_list.append(Payoff(strike=strike, barrier=barrier, S=S_matrix[0]))
     return X_list, Y_list, dYdX_list
+
+
+def normalize_data(X: list, Y: list, dYdX: list):
+    """
+    Inputs:
+     - training samples (1D array)
+     - labels (1D array)
+     - pathwise differentials (1D array)
+    Outputs:
+     - training samples mean (float)
+     - training samples stdev (float)
+     - normalized training samples (1D array)
+     - labels mean (float)
+     - labels stdev (float)
+     - labels samples (1D array)
+     - normalized pathwise differentials (1D array)
+     - differential weights of the cost function (float)
+    """
+    mean_X = np.mean(X)
+    std_X = np.std(X)
+    norm_X = (X - mean_X) / std_X
+    mean_Y = np.mean(Y)
+    std_Y = np.std(Y)
+    norm_Y = (Y - mean_Y) / std_Y
+    mean_dYdX = np.mean(dYdX)
+    std_dYdX = np.std(dYdX)
+    norm_dYdX = (dYdX - mean_dYdX) / std_dYdX
+    lambda_j = 1 / np.sqrt((1/len(dYdX)) * sum(np.power(norm_dYdX, 2)))
+    return mean_X, std_X, norm_X, mean_Y, std_Y, norm_Y, norm_dYdX, lambda_j
