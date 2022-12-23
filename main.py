9e7eae3ce9e23132eed2464ca702fe735bc85190
Theo Joignant <tjoignant@gmail.com>
2022-12-23 19:55:40 +0100

    Test

--- main.py
+++ main.py
@@ -26,9 +26,8 @@ nb_simuls = 1000
 # PART 1: Pricing and hedging by Monte Carlo
 
 # Heston Diffusion
-S_test, V_test = GeneratePathsHestonEuler(S0=S0, v0=v0, risk_free_rate=risk_free_rate, maturity=maturity, rho=rho,
-                                          kappa=kappa, theta=theta, sigma=sigma, nb_steps=nb_steps, nb_simuls=nb_simuls,
-                                          seed=seed)
+S_test = GeneratePathsHestonEuler(S0=S0, v0=v0, risk_free_rate=risk_free_rate, maturity=maturity, rho=rho, kappa=kappa,
+                                  theta=theta, sigma=sigma, nb_steps=nb_steps, nb_simuls=nb_simuls, seed=seed)
 print(S_test)
 
 # Payoff / Pricing
@@ -36,9 +35,9 @@ payoff = Payoff(strike=strike, barrier=barrier, S=S_test[0])
 price = MC_Pricing(strike=strike, barrier=barrier, S0=S0, v0=v0, risk_free_rate=risk_free_rate, maturity=maturity,
                    rho=rho, kappa=kappa, theta=theta, sigma=sigma, nb_steps=nb_steps, nb_simuls=nb_simuls, seed=seed)
 print("\nDiffusion")
-print(f" - Average S_T: {round(np.mean(S_test[:, -1]), 4)}")
+print(f" - Average S_T: {np.mean(S_test[:, -1])}")
 print(f" - Payoff Simul_0: {payoff}")
-print(f" - Payoff Price: {round(price, 4)}")
+print(f" - Payoff Price: {price}")
 
 # FD Greeks
 delta = DeltaFD(strike=strike, barrier=barrier, S0=S0, v0=v0, risk_free_rate=risk_free_rate, maturity=maturity,
@@ -47,12 +46,10 @@ gamma = GammaFD(strike=strike, barrier=barrier, S0=S0, v0=v0, risk_free_rate=ris
                 rho=rho, kappa=kappa, theta=theta, sigma=sigma, nb_steps=nb_steps, nb_simuls=nb_simuls, seed=seed)
 rho = RhoFD(strike=strike, barrier=barrier, S0=S0, v0=v0, risk_free_rate=risk_free_rate, maturity=maturity,
             rho=rho, kappa=kappa, theta=theta, sigma=sigma, nb_steps=nb_steps, nb_simuls=nb_simuls, seed=seed)
-print("\nADD Greeks")
+print("\nFD Greeks")
 print(f" - Delta: {delta}")
 print(f" - Gamma: {gamma}")
 print(f" - Rho: {rho}")
-
-# ADD Greeks
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -65,19 +62,38 @@ X, Y, dYdX = LSM_dataset(strike=strike, barrier=barrier, v0=v0, risk_free_rate=r
 
 # MC Dataset
 MC_prices = []
-Delta_values = []
-Gamma_values = []
-Rho_values = []
+FD_deltas = []
+FD_gammas = []
+FD_rhos = []
+S0_list = np.linspace(10, 200, 40)
+for S_0 in S0_list:
+    MC_prices.append(MC_Pricing(strike=strike, barrier=barrier, S0=S_0, v0=v0, risk_free_rate=risk_free_rate,
+                                maturity=maturity, rho=rho, kappa=kappa, theta=theta, sigma=sigma, nb_steps=nb_steps,
+                                nb_simuls=nb_simuls, seed=seed))
+    FD_deltas.append(DeltaFD(strike=strike, barrier=barrier, S0=S_0, v0=v0, risk_free_rate=risk_free_rate,
+                             maturity=maturity, rho=rho, kappa=kappa, theta=theta, sigma=sigma, nb_steps=nb_steps,
+                             nb_simuls=nb_simuls, seed=seed))
+    FD_gammas.append(GammaFD(strike=strike, barrier=barrier, S0=S_0, v0=v0, risk_free_rate=risk_free_rate,
+                             maturity=maturity, rho=rho, kappa=kappa, theta=theta, sigma=sigma, nb_steps=nb_steps,
+                             nb_simuls=nb_simuls, seed=seed))
+    FD_rhos.append(RhoFD(strike=strike, barrier=barrier, S0=S_0, v0=v0, risk_free_rate=risk_free_rate,
+                         maturity=maturity, rho=rho, kappa=kappa, theta=theta, sigma=sigma, nb_steps=nb_steps,
+                         nb_simuls=nb_simuls, seed=seed))
 
-for temp_S_0 in np.linspace(10, 200):
-    temp_price = MC_Pricing(strike=strike, barrier=barrier, S0=temp_S_0, v0=v0, risk_free_rate=risk_free_rate,
-                            maturity=maturity, rho=rho, kappa=kappa, theta=theta, sigma=sigma, nb_steps=nb_steps,
-                            nb_simuls=nb_simuls, seed=seed)
-    MC_prices.append(temp_price)
+# Fig 1: Pricing Function
+fig1, ax1 = plt.subplots(figsize=(15, 7.5))
+ax1.scatter(X, Y, marker="+", color="grey", label='LSM samples')
+ax1.plot(S0_list, MC_prices, marker="o", color="green", label='MC pricing')
+ax1.set_title('Heston D&O Call Pricing Function')
+ax1.legend()
 
-# Plot Pricing Function
-plt.scatter(X, Y, marker="+", color="grey", label='LSM samples')
-plt.plot(np.linspace(10, 200), MC_prices, marker="o", color="green", label='MC pricing')
-plt.title('Heston D&O Call Pricing Function')
-plt.legend()
+# Fig 2: FD Greeks
+fig2, ax2 = plt.subplots(figsize=(15, 7.5))
+ax2.plot(S0_list, FD_deltas, marker="o", label='Delta')
+ax2.plot(S0_list, FD_gammas, marker="o", label='Gamma')
+ax2.plot(S0_list, FD_rhos, marker="o", label='Rho')
+ax2.set_title('FD Greeks')
+ax2.legend()
+
+# Show Graphs
 plt.show()
