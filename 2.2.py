284a0d932e60a7f78016b4eb93ac575bfe4bc0d1
jmhanna92 <jean-marc.hanna@edu.devinci.fr>
2022-12-30 15:14:41 +0100

    début partie 2.2

--- 2.2.py
+++ 2.2.py
@@ -2,8 +2,8 @@ import torch
 from torch import nn
 
 
-class Net(nn.Module):
-    def __init__(self,nb_inputs,hidL,nb_neurones):
+class Twin_Network(nn.Module):
+    def __init__(self,nb_inputs,hidL=4,nb_neurones=20):
         w=torch.empty(3,5)
         nn.init.kaiming_normal_(w,mode='fan_out',nonlinearity='relu')
         super().__init__()
@@ -13,30 +13,17 @@ class Net(nn.Module):
         self.hidden1 = nn.Linear(nb_inputs, nb_neurones)
         self.hidden2 = nn.Linear(nb_neurones,nb_neurones)
         self.hidden3 = nn.Linear(nb_neurones,nb_neurones)
-        self.output = nn.Linear(nb_neurones, 1)
+        self.hidden4 = nn.Linear(nb_neurones, nb_neurones)
+        self.output=nn.Linear(nb_neurones,1)
 
         def forward(self, x):
             x = F.relu(self.hidden1(x))
             x = F.relu(self.hidden2(x))
             x = F.relu(self.hidden3(x))
-            #x = F.relu(self.output(x))
+            x = F.relu(self.hidden4(x))
+            x=self.output(x)
 
             return x
 
-net = Net(20,4,20)
-print(net)
-        # Couche de sortie, 10 unités - une pour chaque digit
-        #self.output = nn.Linear(256, 10)
-
-        # Définir l'activation sigmoïde et la sortie softmax
-    #    self.relu = nn.ReLU()
-    #    self.softmax = nn.ReLU(dim=1)
-
-    #def forward(self, x):
-        # Passez le tenseur d'entrée à travers chacune de nos opérations
-    #    x = self.hidden(x)
-    #    x = self.relu(x)
-    #    x = self.output(x)
-    #    x = self.softmax(x)
-
-    #    return x
+twin_Network = Twin_Network(20,4,20)
+print(twin_Network)
