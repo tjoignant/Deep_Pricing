71978926c7b905d3b3aafaf7f28cb5b65f3a05ab
Theo Joignant <tjoignant@gmail.com>
2022-12-31 09:09:29 +0100

    Update NN Class Initialization

--- class_nn.py
+++ class_nn.py
@@ -5,18 +5,23 @@ import torch.nn as nn
 class Twin_Network(nn.Module):
     def __init__(self, nb_inputs, nb_hidden_layer, nb_neurones):
         super(Twin_Network, self).__init__()
-        # Layers Initialization
+        self.nb_inputs = nb_inputs
+        self.nb_hidden_layer = nb_hidden_layer
+        self.nb_neurones = nb_neurones
         self.layers = []
-        for i in range(0, nb_hidden_layer + 1):
+        self._init_layers()
+
+    def _init_layers(self):
+        for i in range(0, self.nb_hidden_layer + 1):
             # If First Layer
             if i == 0:
-                layer = nn.Linear(nb_inputs, nb_neurones)
+                layer = nn.Linear(self.nb_inputs, self.nb_neurones)
             # If Output Layer
-            elif i == nb_hidden_layer:
-                layer = nn.Linear(nb_neurones, 1)
+            elif i == self.nb_hidden_layer:
+                layer = nn.Linear(self.nb_neurones, 1)
             # If Hidden Layer
             else:
-                layer = nn.Linear(nb_neurones, nb_neurones)
+                layer = nn.Linear(self.nb_neurones, self.nb_neurones)
             # He Initialization
             nn.init.kaiming_normal_(layer.weight, mode='fan_in')
             self.layers.append(layer)
@@ -49,8 +54,16 @@ class Twin_Network(nn.Module):
         dYdX = dYdX_norm * dYdX_std + dYdX_mean
         return Y, dYdX
 
+    def training(self, X_norm, Y_norm, lambda_j, nb_epochs, dYdX_norm=None):
+        # Case 1 : Training on Samples & Differentials
+        if dYdX_norm:
+            return None
+        # Case 2 : Training on Samples
+        else:
+            return None
+
 
 if __name__ == '__main__':
     torch.manual_seed(123)
     twin_network = Twin_Network(nb_inputs=20, nb_hidden_layer=4, nb_neurones=20)
-    print(twin_network)
+    print(twin_network.nb_inputs)
