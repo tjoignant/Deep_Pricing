33510a73857ecef022b7ba3af4191ee596edc3b1
Theo Joignant <tjoignant@gmail.com>
2023-01-03 21:50:53 +0100

    Updated Functions Documentation

--- NN_functions.py
+++ NN_functions.py
@@ -30,7 +30,13 @@ class Twin_Network(nn.Module):
             nn.init.kaiming_normal_(layer.weight, mode='fan_in')
             self.layers.append(layer)
 
-    def forward(self, x):
+    def forward(self, x: float):
+        """
+        Inputs:
+         - x              : input (float)
+        Outputs:
+         - propagated input (float)
+        """
         # Set Activation Function
         activation = nn.ReLU()
         # For Each Layer
@@ -45,7 +51,17 @@ class Twin_Network(nn.Module):
                 x = activation(layer(x))
         return x
 
-    def predict_price(self, X, X_mean, X_std, Y_mean, Y_std):
+    def predict_price(self, X: float, X_mean: float, X_std: float, Y_mean: float, Y_std: float):
+        """
+        Inputs:
+         - X              : initial asset spot (float)
+         - X_mean         : training samples mean (float)
+         - X_std          : training samples stdev (float)
+         - Y_mean         : training labels mean (float)
+         - Y_std          : training labels stdev (float)
+        Outputs:
+         - predicted price (float)
+        """
         # Forward Propagation
         X = torch.tensor([X])
         X_norm = torch.div(X - X_mean, X_std)
@@ -53,7 +69,21 @@ class Twin_Network(nn.Module):
         Y = Y_norm * Y_std + Y_mean
         return Y
 
-    def predict_price_and_diffs(self, X, X_mean, X_std, Y_mean, Y_std, dYdX_mean, dYdX_std):
+    def predict_price_and_diffs(self, X: float, X_mean: float, X_std: float, Y_mean: float, Y_std: float,
+                                dYdX_mean: float, dYdX_std: float):
+        """
+        Inputs:
+         - X              : initial asset spot (float)
+         - X_mean         : training samples mean (float)
+         - X_std          : training samples stdev (float)
+         - Y_mean         : training labels mean (float)
+         - Y_std          : training labels stdev (float)
+         - dYdX_mean      : training differentials mean (float)
+         - dYdX_std       : training differentials stdev (float)
+        Outputs:
+         - predicted payoff (float)
+         - predicted delta (float)
+        """
         # Forward Propagation
         X = torch.tensor([X], requires_grad=True)
         X_norm = torch.div(X - X_mean, X_std)
@@ -66,7 +96,15 @@ class Twin_Network(nn.Module):
         return Y, dYdX
 
 
-def MSE_standard(model, X_norm, Y_norm):
+def MSE_standard(model: Twin_Network, X_norm: list, Y_norm: list):
+    """
+    Inputs:
+     - model          : neural network (Twin_Network())
+     - X_norm         : training samples normalized (1D tensor)
+     - Y_norm         : training labels normalized (1D tensor)
+    Outputs:
+     - standard mean squared error (float)
+    """
     loss = torch.tensor(0.0)
     for x, y in zip(X_norm, Y_norm):
         x = torch.tensor([x])
@@ -75,7 +113,18 @@ def MSE_standard(model, X_norm, Y_norm):
     return loss
 
 
-def MSE_differential(model, X_norm, Y_norm, dYdX_norm, lambda_j, alpha):
+def MSE_differential(model: Twin_Network, X_norm: list, Y_norm: list, dYdX_norm: list, lambda_j: float, alpha: float):
+    """
+    Inputs:
+     - model          : neural network (Twin_Network())
+     - X_norm         : training samples normalized (1D tensor)
+     - Y_norm         : training labels normalized (1D tensor)
+     - dYdX_norm      : training differentials normalized (1D tensor)
+     - lambda_j       : cost function differential weight (float)
+     - alpha          : cost function differential factor (float)
+    Outputs:
+     - differential mean squared error (float)
+    """
     loss = alpha * MSE_standard(model, X_norm, Y_norm)
     if alpha != 1:
         ds = pow(10, -4)
@@ -89,7 +138,18 @@ def MSE_differential(model, X_norm, Y_norm, dYdX_norm, lambda_j, alpha):
     return loss
 
 
-def training(model, X_norm, Y_norm, nb_epochs, dYdX_norm=None, lambda_j=None):
+def training(model: Twin_Network, X_norm: list, Y_norm: list, nb_epochs: int, dYdX_norm=None, lambda_j=None):
+    """
+    Inputs:
+     - model          : neural network (Twin_Network())
+     - X_norm         : normalized training samples (1D tensor)
+     - Y_norm         : normalized training labels (1D tensor)
+     - nb_epochs      : number of training iterations (int)
+     - dYdX_norm      : normalized pathwise differentials (1D tensor)
+     - lambda_j       : cost function differential weight (float)
+    Outputs:
+     - trained model (Twin_Network())
+    """
     # Cost Function
     if dYdX_norm is None:
         alpha = 1
